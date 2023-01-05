ab38d490a962b224fe1e7650cf8d479a65a092e7
minhnc912 <minhnguyen91200@gmail.com>
2023-01-05 10:43:24 +0700

    a

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
